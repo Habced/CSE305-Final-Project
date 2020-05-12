62628702e6b704b9cdf64c5e2143d0630aa8c3a4
unknown <seungyboy@gmail.com>
2020-05-12 22:52:03 +0900

    Database connection test

--- index.php
+++ index.php
@@ -8,13 +8,42 @@
     <meta content="" name="keywords">
 
     <?php 
-      include 'include/dbConn.php'
+      include 'include/dbConn.php';
     ?>
   </head>
   <body>
     index.php is working!<br>
     <?php 
-      echo "echo from php is working!";
+      echo "echo from php is working!<br>Testing!";
+
+      
+      // SQL and QUERY example
+      // $sql = "SELECT * FROM Products";
+      // $query = mysqli_query($conn, $sql) or die ( mysqli_error($conn));
+      // while( $row = mysqli_fetch_array($query)) {
+      //   echo "<br>" . $row['model'] . "<br>";
+      // }
+      
+      // Print out all $row
+      // foreach($row as $cname => $cvalue){
+      //   print "$cname: $cvalue\t";
+      // }
+      // print "\r\n";
+
+      
+      $sql = "SELECT * FROM playground_tests";
+      $query = mysqli_query($conn, $sql) or die ( mysqli_error($conn));
+      while( $row = mysqli_fetch_array($query)) {
+        echo "<br>" . $row['content'] . "<br>";
+        foreach($row as $cname => $cvalue){
+          print "$cname: $cvalue\t";
+        }
+      }
+      
     ?>
+
+
+
+    <?php include 'include/EndDbConn.php';?>
   </body>
 </html>
